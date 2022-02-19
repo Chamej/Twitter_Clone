9b8b88c7553d1ca42cb4e881fbe1d574a46a29fb
Chamej <chamej.awad@gmail.com>
2022-02-19 21:23:53 +0100

    added form

--- twitter/views.py
+++ twitter/views.py
@@ -1,11 +1,21 @@
 from multiprocessing import context
 from django.shortcuts import render, redirect
 from .models import Profile, Post
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, PostForm
 
 def home(request):
     posts = Post.objects.all()
-    context = {'posts':posts}
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+
+    context = {'posts':posts, 'form':form}
     return render(request, 'twitter/newsfeed.html')
 
 def register(request):
