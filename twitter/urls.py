031c0b36d7b45c81be0b041c40c75670ff34cf47
Chamej <chamej.awad@gmail.com>
2022-02-19 23:10:44 +0100

    added follow option

--- twitter/urls.py
+++ twitter/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='twitter/login.html'), name='login'),
     path('logout/', LoginView.as_view(template_name='twitter/logout.html'), name='logout'),
     path('delete/<int:post_id>/', views.delete, name='delete'),
-    path('profile/<str:username>/', views.profile, name='profile')
+    path('profile/<str:username>/', views.profile, name='profile'),
     path('editar/', views.editar, name='editar'),
+    path('follow/<str:username>/'), views.follow, name='follow'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
