a4f8659ba61b91f03f17b3e3b848f2cf1a3a0437
Chamej <chamej.awad@gmail.com>
2022-02-19 23:28:00 +0100

    added unfollow option and fixed bugs

--- twitter/urls.py
+++ twitter/urls.py
@@ -6,15 +6,14 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='twitter/login.html'), name='login'),
-    path('logout/', LoginView.as_view(template_name='twitter/logout.html'), name='logout'),
-    path('delete/<int:post_id>/', views.delete, name='delete'),
-    path('profile/<str:username>/', views.profile, name='profile'),
-    path('editar/', views.editar, name='editar'),
-    path('follow/<str:username>/'), views.follow, name='follow'),
+	path('', views.home, name='home'),
+	path('register/', views.register, name='register'),
+	path('login/', LoginView.as_view(template_name='twitter/login.html'), name='login'),
+	path('logout/', LogoutView.as_view(), name='logout'),
+	path('delete/<int:post_id>/', views.delete, name='delete'),
+	path('profile/<str:username>/', views.profile, name='profile'),
+	path('editar/', views.editar, name='editar'),
+	path('follow/<str:username>/', views.follow, name='follow'),
+	path('unfollow/<str:username>/', views.unfollow, name='unfollow'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
