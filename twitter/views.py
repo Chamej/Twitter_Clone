573e6b6fe02160ba1a3ffd542282be33dd438f7c
Chamej <chamej.awad@gmail.com>
2022-02-19 21:02:24 +0100

    v2

--- twitter/views.py
+++ twitter/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
 from .models import Profile, Post
+from .forms import UserRegisterForm
 
 def home(request):
     posts = Post.objects.all()
     context = {'posts':posts}
     return render(request, 'twitter/newsfeed.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    context = {'form' : form}
+    return render(request, 'twitter/register.html', context)
